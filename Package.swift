14355e5a2f9d169423eeb6f4e8b8a3a18552cd2d
sergdort <sergdort@gmail.com>
2016-09-29 21:51:27 +0100

    Use fork

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporMySQL",
     dependencies: [
-   		.Package(url: "https://github.com/vapor/mysql-driver.git", majorVersion: 1),
+   		.Package(url: "https://github.com/sergdort/mysql-driver.git", majorVersion: 1),
    		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
     ]
 )
